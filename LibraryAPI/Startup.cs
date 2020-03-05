a14aa2d4b7440e7d5d4d8f837a1695502c6f320d
jaqb8 <qba.anisz@gmail.com>
2020-03-05 19:34:14 +0100

    code cleanup

--- LibraryAPI/Startup.cs
+++ LibraryAPI/Startup.cs
@@ -19,7 +19,6 @@ namespace LibraryAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<BookContext>(opt =>
@@ -32,7 +31,6 @@ namespace LibraryAPI
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
